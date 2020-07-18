35021e2d570c45d44ee05a8c4ceca551b63f2de1
Alessandro Dalle Sasse <a.dallesasse@datasoil.it>
2020-07-18 17:13:18 +0200

    Exported functions of the therapeutic touch example

--- src/PuppyBook.jl
+++ src/PuppyBook.jl
@@ -2,9 +2,12 @@ module PuppyBook
 
     include("03_running_proportion.jl")
     include("04_bayes_update.jl")
+    include("therapeutic_touch.jl")
 
     export compute_running_proportion, plot_running_proportion,
-        coinflip_discrete_bayes_update, plot_coinflip_discrete_bayes_update
+        coinflip_discrete_bayes_update, plot_coinflip_discrete_bayes_update,
+        load_therapeutic_touch_data, tt_sample_basic_model, tt_sample_hierarchical_model,
+        plot_tt_shrinkage_effect
     
     greet() = print("Hello Puppies!")
 
